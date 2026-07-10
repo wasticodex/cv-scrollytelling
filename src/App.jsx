c6132a7932e5b8c3980c42432f5144f8fbce692d
wasti.codex <wasti.codex@gmail.com>
2026-07-10 23:01:37 +0200

    first step - html et css test simple

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+function App() {
+  return (
+    <div className="cv">
+      <section id="hero">
+        <h1>Matthieu PIRON</h1>
+        <h2>Développeur Web Full Stack</h2>
+        <p>En formation</p>
+        <a href="#apropos">↓ Découvrir</a>
+      </section>
+      
+      <section id="a propos">
+        <h2>À propos</h2>
+      </section>
+
+      <section id="formation">
+        <h2>Formation</h2>
+      </section>
+
+      <section id="competences">
+        <h2>Projets</h2>
+      </section>
+
+      <section id="experiences">
+        <h2>Expériences</h2>
+      </section>
+
+      <section id="contact">
+        <h2>Contact</h2>
+      </section>
+    </div>
+  )
+}
+
+export default App

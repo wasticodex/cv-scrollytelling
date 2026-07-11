e135664160f750c909d764edebb6ad6bca470eb5
wasti.codex <wasti.codex@gmail.com>
2026-07-11 10:42:21 +0200

    Typewritter et CSS effect GLITCHE

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
+import { useState, useEffect, use } from 'react'
+
+function TypeWritter({ textes }) {
+  const [index, setIndex] = useState(0)
+  const [texte, setTexte] = useState('')
+  const [i, setI] = useState(0)
+
+  useEffect(() => {
+    if (i < textes[index].length) {
+      const timeout = setTimeout(() => {
+        setTexte(t => t + textes[index][i])
+        setI(i => i + 1)
+      }, 100)
+    return () => clearTimeout(timeout)
+    } else {
+      setTimeout(() => {
+        setTexte('')
+        setI(0)
+        setIndex(idx => (idx + 1) % textes.length)
+      }, 2000)
+    }
+  }, [i, index])
+
+  return <span>{texte}<span className='cursor'>|</span></span>
+}
+
 function App() {
   return (
     <div className="cv">
       <section id="hero">
         <h1>Matthieu PIRON</h1>
-        <h2>Développeur Web Full Stack</h2>
+        <h2>
+          <TypeWritter textes={[
+            "Développeur Web Full Stack",
+            "En formation autodidacte",
+            "Designer numérique polyvalent"
+          ]} /> 
+        </h2>
         <p>En formation</p>
         <a href="#apropos">↓ Découvrir</a>
       </section>
